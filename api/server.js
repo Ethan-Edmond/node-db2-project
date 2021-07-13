beecc1c1714444e26be0682a7872cda2b221544c
Ethan Edmond <eedmond@uccs.edu>
2021-07-13 14:46:37 -0600

    Added a little error handler, npm tests all pass

--- api/server.js
+++ api/server.js
@@ -8,4 +8,11 @@ server.use(express.json());
 
 server.use('/api/cars', carsRouter);
 
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  });
+});
+
 module.exports = server;
